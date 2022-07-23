a46216809c861884991eadb57002a45a2127b789
eswise <eswise@gmail.com>
2022-07-23 14:25:33 -0400

    My First Variable
    Finished Project

---
+++ MyFirstVariable/Program.cs
@@ -0,0 +1,18 @@
+﻿// single line comment
+
+/* Multiple
+ * Line
+ * Comment
+ */
+
+// Declare a variable
+string name;
+
+// Write() does not return to next line, WriteLine() does
+Console.Write("Enter your name: ");
+
+// ReadLine() takes text input from the keyboard until the user hits <enter> / <return>
+name = Console.ReadLine();
+
+// Adding two strings (+) concatenates them
+Console.WriteLine("Hello, " + name);
