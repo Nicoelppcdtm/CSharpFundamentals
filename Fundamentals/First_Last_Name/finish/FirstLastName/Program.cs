b0dd4e0f7a5cab2c53de7324f062f51a604f35d3
eswise <eswise@gmail.com>
2022-07-25 18:06:48 -0400

    First Last Name Exercise

---
+++ Fundamentals/First_Last_Name/finish/FirstLastName/Program.cs
@@ -0,0 +1,14 @@
+﻿// declare two string variables
+string firstName;
+string lastName;
+
+// prompt for first name using Write() to keep it on the same line
+Console.Write("Enter First Name: ");
+firstName = Console.ReadLine();
+
+// same for last name
+Console.Write("Enter Last Name: ");
+lastName = Console.ReadLine();
+
+// print message in format specified by the exercise
+Console.WriteLine("You have entered the name " + lastName + ", " + firstName + ".");
