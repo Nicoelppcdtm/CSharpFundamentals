e717e648389aeba29f0bf05d0e76655ec0d2b5f4
eswise <eswise@gmail.com>
2022-08-01 09:09:57 -0400

    Added comments

--- Fundamentals/First_Last_Name/finish/FirstLastName/Program.cs
+++ Fundamentals/First_Last_Name/finish/FirstLastName/Program.cs
@@ -4,11 +4,12 @@ string lastName;
 
 // prompt for first name using Write() to keep it on the same line
 Console.Write("Enter First Name: ");
+// use ReadLine() to get the name and assign its value to the variable
 firstName = Console.ReadLine();
 
-// same for last name
+// same for last name... Write() and ReadLine()
 Console.Write("Enter Last Name: ");
 lastName = Console.ReadLine();
 
-// print message in format specified by the exercise
+// Use WriteLine() to output message in format specified by the exercise
 Console.WriteLine("You have entered the name " + lastName + ", " + firstName + ".");
